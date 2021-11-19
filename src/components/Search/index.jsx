b14d035038d8ba5e195ded91d7a6ad1e4fe2defd
cindrhuang <cindyrhuang@cmail.carleton.ca>
2021-11-18 19:26:13 -0500

    added search feature, order, login page

---
+++ src/components/Search/index.jsx
@@ -0,0 +1,8 @@
+import "./styles.css";
+
+export const Search = (props) => {
+    return (
+        <input name="search" placeholder="Search by breed..." type="text" onChange={props.handleSearchUpdate}/>
+
+    )
+}
